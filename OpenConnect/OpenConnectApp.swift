13e2fcb399183e7d4c6d4986be1c63ee9a505b80
NurAhmadullah <sohag.cse14.hstu@gmail.com>
2023-04-07 19:03:29 +0600

    empty project with coredata

---
+++ OpenConnect/OpenConnectApp.swift
@@ -0,0 +1,20 @@
+//
+//  OpenConnectApp.swift
+//  OpenConnect
+//
+//  Created by Sohag Macbook Pro on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenConnectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
