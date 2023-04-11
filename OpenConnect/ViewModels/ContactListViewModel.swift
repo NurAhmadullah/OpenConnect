5996aa6155bd6c23e809f017844b37436a77bd98
nur.ahmadullah <nur.ahmadullah@bjitgroup.com>
2023-04-11 09:54:18 +0600

    MVVM folder created

---
+++ OpenConnect/ViewModels/ContactListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  OpenConnect
+//
+//  Created by BJIT on 11/4/23.
+//
+
+import Foundation
+
+struct ContactListViewModel{
+    
+}
---
+++ OpenConnect/ViewModels/ContactViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContactViewModel.swift
+//  OpenConnect
+//
+//  Created by BJIT on 11/4/23.
+//
+
+import Foundation
+
+struct ContactViewModel{
+    
+}
